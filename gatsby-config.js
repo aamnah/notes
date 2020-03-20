2b8ae97c4b8c77937961fe781a0b5d016de1ac28
Aamnah <hello@aamnah.com>
2020-03-20 11:25:24 +0500

    removed trailing slashes
    in an attempt to resolve `The path passed to gatsby-source-filesystem does not exist on your file system`

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,21 +42,21 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `${__dirname}/src/pages/`,
+        path: `${__dirname}/src/pages`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
-        path: `${__dirname}/src/posts/`,
+        path: `${__dirname}/src/posts`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `data`,
-        path: `${__dirname}/src/data/`,
+        path: `${__dirname}/src/data`,
         ignore: [`**/\.*`], // ignore files starting with a dot
       },
     },
