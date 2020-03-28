d396d3c857fc482754cd5da7a32c016fd5b35233
Aamnah <hello@aamnah.com>
2020-03-28 22:22:35 +0500

    still the contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,27 +7,27 @@ export default function Contact() {
   return (
     <div id="contact">
       <h1>Send me a message</h1>
-      <form name="Contact" method="POST" data-netlify-honeypot="bot-field" data-netlify="true">
-        <input type="hidden" name="form-name" value="Aamnah.com Contact" />
+      <form name="Contact" method="POST" data-netlify-honeypot="bot-field" netlify data-netlify="true">
+        <Input type="hidden" name="form-name" value="Aamnah.com Contact" />
         <InputGroup>
           <FormInput>
             <Label htmlFor="name">Name</Label>
-            <input name="name" type="text" placeholder="Henry Cavill" id="name" />
+            <Input name="name" type="text" placeholder="Henry Cavill" id="name" />
           </FormInput>
           <FormInput>
             <Label htmlFor="phone">Phone (optional)</Label>
-            <input name="phone" type="phone" placeholder="+1 (123) 1234567" id="phone" />
+            <Input name="phone" type="phone" placeholder="+1 (123) 1234567" id="phone" />
           </FormInput>
         </InputGroup>
 
         <InputGroup>
           <FormInput>
             <Label htmlFor="email">Email</Label>
-            <input name="email" type="email" placeholder="you@yourdomain.com" id="email" />
+            <Input name="email" type="email" placeholder="you@yourdomain.com" id="email" />
           </FormInput>
           <FormInput>
             <Label htmlFor="website">Website (optional)</Label>
-            <input name="website" type="text" placeholder="www.domain.com" id="website" />
+            <Input name="website" type="text" placeholder="www.domain.com" id="website" />
           </FormInput>
         </InputGroup>
 
--- src/components/common/Form/Input.jsx
+++ src/components/common/Form/Input.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
 import './Input.scss'
-export function Input({ type = 'text', placeholder = 'placeholder', className }) {
-  return <input type={type} placeholder={placeholder} className={`${className} Input`} />
+export function Input({ type = 'text', placeholder = 'placeholder', className, ...rest }) {
+  return <input type={type} placeholder={placeholder} className={`${className} Input`} value={value} {...rest} />
 }
