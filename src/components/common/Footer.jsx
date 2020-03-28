49dbd330c0f4116355a6296aab38d6ce9ed1a854
Aamnah <hello@aamnah.com>
2020-03-28 16:49:27 +0500

    ~ Footer text and styles

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -5,10 +5,17 @@ import { Link } from './Link'
 export function Footer() {
   return (
     <footer className="Footer">
-      Coded with love and built using{' '}
-      <Link external to="https://www.gatsbyjs.org">
-        Gatsby
-      </Link>
+      {' '}
+      <p>
+        Please note that this site and the posts on it are, and will always be, a work in progress. If i waited for
+        perfection, i’d never get anything done.{' '}
+      </p>
+      <small>
+        Coded with love and built using{' '}
+        <Link external to="https://www.gatsbyjs.org">
+          Gatsby
+        </Link>
+      </small>
     </footer>
   )
 }
