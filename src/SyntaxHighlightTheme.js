9893e6990d52a195645a9f42c7ed28f7b8f585db
Aamnah <hello@aamnah.com>
2020-05-01 03:44:39 +0500

    ~ updated colors

--- src/SyntaxHighlightTheme.js
+++ src/SyntaxHighlightTheme.js
@@ -1,15 +1,26 @@
-let colors = {}
+let Color = {
+  color: '#393A34',
+  background: '#f5f2f0',
+  comment: '#999988',
+  string: '#690',
+  punctuation: '#393A34',
+  property: '#1BBC9B',
+  atrule: '#07a',
+  function: '#ff6400',
+  color9: '#6f42c1',
+  keyword: '#905',
+}
 
 var theme = {
   plain: {
-    color: '#393A34',
-    backgroundColor: '#f6f8fa',
+    color: Color.color,
+    backgroundColor: Color.background,
   },
   styles: [
     {
       types: ['comment', 'prolog', 'doctype', 'cdata'],
       style: {
-        color: '#999988',
+        color: Color.comment,
         fontStyle: 'italic',
       },
     },
@@ -22,43 +33,43 @@ var theme = {
     {
       types: ['string', 'attr-value'],
       style: {
-        color: '#e3116c',
+        color: Color.string,
       },
     },
     {
       types: ['punctuation', 'operator'],
       style: {
-        color: '#393A34',
+        color: Color.punctuation,
       },
     },
     {
       types: ['entity', 'url', 'symbol', 'number', 'boolean', 'variable', 'constant', 'property', 'regex', 'inserted'],
       style: {
-        color: '#36acaa',
+        color: Color.property,
       },
     },
     {
       types: ['atrule', 'keyword', 'attr-name', 'selector'],
       style: {
-        color: '#00a4db',
+        color: Color.atrule,
       },
     },
     {
       types: ['function', 'deleted', 'tag'],
       style: {
-        color: '#d73a49',
+        color: Color.function,
       },
     },
     {
       types: ['function-variable'],
       style: {
-        color: '#6f42c1',
+        color: Color.color9,
       },
     },
     {
       types: ['tag', 'selector', 'keyword'],
       style: {
-        color: '#00009f',
+        color: Color.keyword,
       },
     },
   ],
