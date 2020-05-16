4ddf69c9d586f04c477c298c5ee8667ef5c144cd
Aamnah <hello@aamnah.com>
2020-05-16 23:14:15 +0500

    - deleted cz not using

--- src/components/common/PostLink.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Link } from 'gatsby'
-
-export function PostLink({ post }) {
-  let { title, path, name } = post
-  return <Link to={path}>{title !== '' ? title : post.name}</Link>
-}
