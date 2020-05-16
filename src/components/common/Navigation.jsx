7ae20635d5cb5b1acc4e87f5cb228ac163ec9f27
Aamnah <hello@aamnah.com>
2020-05-16 21:00:24 +0500

    ~ updated Navigation links

--- src/components/common/Navigation.jsx
+++ src/components/common/Navigation.jsx
@@ -5,22 +5,35 @@ import './Navigation.scss'
 export function Navigation() {
   return (
     <div className="Navigation">
-      <Link className="Navigation-item" to="/">
-        Home
-      </Link>
-      <Link className="Navigation-item" to="/#contact">
-        Contact
-      </Link>
-      <Link className="Navigation-item" to="/blog">
-        Blog
-      </Link>
-      {/* <Link className="Navigation-item" to="/notes">
+      <div className="Navigation-left">
+        <Link className="Navigation-item" to="/">
+          Home
+        </Link>
+        <Link className="Navigation-item" to="/#contact">
+          Contact
+        </Link>
+        <Link className="Navigation-item" to="/blog">
+          Blog
+        </Link>
+        {/* <Link className="Navigation-item" to="/notes">
         Notes
       </Link> */}
 
-      <Link external className="Navigation-item" to="https://notes.aamnah.com">
-        Notes
-      </Link>
+        <Link external className="Navigation-item" to="https://notes.aamnah.com">
+          Notes
+        </Link>
+      </div>
+      <div className="Navigation-right">
+        <Link external className="Navigation-item" to="https://www.behance.net/aamnah">
+          Behance
+        </Link>
+        <Link external className="Navigation-item" to="https://www.instagram.com/aamnahakram/">
+          Instagram
+        </Link>
+        <Link external className="Navigation-item" to="https://github.com/aamnah">
+          Github
+        </Link>
+      </div>
     </div>
   )
 }
