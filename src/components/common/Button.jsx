c438845b0a0b341e00c04c9568a59f7b4cbe3c65
Aamnah <hello@aamnah.com>
2020-08-01 17:13:29 +0500

    feat: added icon to <Button>

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 import './Button.scss'
-export function Button({ children }) {
-  return <button className="Button">{children}</button>
+import { Icon } from './Icon'
+export function Button({ type, icon, children, ...rest }) {
+  return (
+    <button type={type ? type : 'submit'} className="Button" {...rest}>
+      {icon ? <Icon name={icon} fill="white" className="Button-icon" /> : null}
+      {children}
+    </button>
+  )
 }
