cc5d4ad129c02e58b83e1a082a4e9e5459963791
Aamnah <hello@aamnah.com>
2020-08-07 09:11:15 +0500

    added some love

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 import './Footer.scss'
 import { Link } from './Link'
+
+import { Icon } from 'components/common'
 export function Footer() {
   return (
     <footer className="Footer">
       {' '}
       <p>
-        Please note that this site and the posts on it are, and will always be,
-        a work in progress. If i waited for perfection, i’d never get anything
-        done.{' '}
+        Please note that this site and the posts on it are, and will always be, a work in progress. If i waited for
+        perfection, i’d never get anything done.{' '}
       </p>
       <div className="Footer-bottom">
         <small>
@@ -21,7 +22,7 @@ export function Footer() {
           <Link external to="https://www.netlify.com/">
             Netlify
           </Link>{' '}
-          and magic!
+          and <Icon name="heart" size={12} fill="var(--color-faded)" /> and magic!
         </small>
 
         <Link to="/rss.xml">RSS</Link>
