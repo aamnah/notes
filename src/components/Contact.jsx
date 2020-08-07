eb64eb58fe38066de73456c5a19e6577b8aa31f0
Aamnah <hello@aamnah.com>
2020-08-07 13:42:44 +0500

    sections

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,54 +32,57 @@ export default function Contact() {
         </Link>
       </Social>
       <p>Use the form below to send me a message..</p>
-      <Form
-        name="Contact-form"
-        method="POST"
-        // action="/thanks/" // this will be our custom Thank You page
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        // onSubmit={handleContactSubmit}
-      >
-        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-        <Input type="hidden" name="form-name" value="Contact-form" />
-        <InputGroup>
-          <FormInput>
-            <Label htmlFor="name">Name</Label>
-            <Input name="name" type="text" placeholder="Henry Cavill" id="name" />
-          </FormInput>
-          <FormInput>
-            <Label htmlFor="email">Email</Label>
-            <Input name="email" type="email" placeholder="henry@cavill.com" id="email" />
-          </FormInput>
-        </InputGroup>
 
-        <InputGroup>
-          <FormInput>
-            <Label htmlFor="phone">Phone (optional)</Label>
-            <Input name="phone" type="phone" placeholder="+1 (123) 1234567" id="phone" />
-          </FormInput>
-          <FormInput>
-            <Label htmlFor="website">Website (optional)</Label>
-            <Input name="website" type="text" placeholder="www.cavill.com" id="website" />
-          </FormInput>
-        </InputGroup>
+      <section>
+        <Form
+          name="Contact-form"
+          method="POST"
+          // action="/thanks/" // this will be our custom Thank You page
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          // onSubmit={handleContactSubmit}
+        >
+          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+          <Input type="hidden" name="form-name" value="Contact-form" />
+          <InputGroup>
+            <FormInput>
+              <Label htmlFor="name">Name</Label>
+              <Input name="name" type="text" placeholder="Henry Cavill" id="name" />
+            </FormInput>
+            <FormInput>
+              <Label htmlFor="email">Email</Label>
+              <Input name="email" type="email" placeholder="henry@cavill.com" id="email" />
+            </FormInput>
+          </InputGroup>
 
-        <InputGroup>
-          <FormInput>
-            <Label htmlFor="message">Message</Label>
-            <Textarea
-              name="message"
-              placeholder="say what you mean, mean what you say"
-              className="Input"
-              id="message"
-            />
-          </FormInput>
-        </InputGroup>
+          <InputGroup>
+            <FormInput>
+              <Label htmlFor="phone">Phone (optional)</Label>
+              <Input name="phone" type="phone" placeholder="+1 (123) 1234567" id="phone" />
+            </FormInput>
+            <FormInput>
+              <Label htmlFor="website">Website (optional)</Label>
+              <Input name="website" type="text" placeholder="www.cavill.com" id="website" />
+            </FormInput>
+          </InputGroup>
 
-        <Button type="submit" className="Button" icon="plane">
-          Send Message
-        </Button>
-      </Form>
+          <InputGroup>
+            <FormInput>
+              <Label htmlFor="message">Message</Label>
+              <Textarea
+                name="message"
+                placeholder="say what you mean, mean what you say"
+                className="Input"
+                id="message"
+              />
+            </FormInput>
+          </InputGroup>
+
+          <Button type="submit" className="Button" icon="plane">
+            Send Message
+          </Button>
+        </Form>
+      </section>
     </div>
   )
 }
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -14,74 +14,78 @@ export default function AboutPage() {
     <DefaultLayout>
       <Head />
       <SEO title="About" />
-
-      <h1>Aamnah</h1>
-      <ul>
-        <li>works as a frontend developer</li>
-        <li>has a degree in Journalism and Maths</li>
-        <li>studied Media Studies at Kinnaird College</li>
-        <li>is fluent in three languages</li>
-        <li>enjoys writing bash scripts</li>
-        <li>
-          balances between work, home and <em>dolce far niente</em>
-        </li>
-        <li>needs coffee</li>
-        <li>loves steak</li>
-        <li>owns a company</li>
-        <li>plays the pink panther theme song on ukulele</li>
-      </ul>
-      <Pullquote>
-        Out of all the things that i do, i particularly enjoy writing Sass stylesheets, designing UI mockups and writing
-        bash scripts.
-      </Pullquote>
-      <h3>Current setup</h3>
-      <ul>
-        <li>Computer: Custom built PC / Lenovo Thinkpad / Macbook Air</li>
-        <li>Editor: Visual Studio Code</li>
-        <li>Host: Netlify</li>
-        <li>Site Generator: Gatsby / Hugo</li>
-      </ul>
+      <section>
+        <h1>Aamnah</h1>
+        <ul>
+          <li>works as a frontend developer</li>
+          <li>has a degree in Journalism and Maths</li>
+          <li>studied Media Studies at Kinnaird College</li>
+          <li>is fluent in three languages</li>
+          <li>enjoys writing bash scripts</li>
+          <li>
+            balances between work, home and <em>dolce far niente</em>
+          </li>
+          <li>needs coffee</li>
+          <li>loves steak</li>
+          <li>owns a company</li>
+          <li>plays the pink panther theme song on ukulele</li>
+        </ul>
+        <Pullquote>
+          Out of all the things that i do, i particularly enjoy writing Sass stylesheets, designing UI mockups and
+          writing bash scripts.
+        </Pullquote>
+      </section>
+      <section>
+        <h3>Current setup</h3>
+        <ul>
+          <li>Computer: Custom built PC / Lenovo Thinkpad / Macbook Air</li>
+          <li>Editor: Visual Studio Code</li>
+          <li>Host: Netlify</li>
+          <li>Site Generator: Gatsby / Hugo</li>
+        </ul>
+      </section>
 
       {/* Add pictures of workspace */}
-      <h3>Current job: Frontend Developer</h3>
-      <p>
-        <Tag>reactjs</Tag> <Tag>react-native</Tag> <Tag>redux</Tag> <Tag>typescript</Tag> <Tag>styled-components</Tag>
-      </p>
-      <p>
-        I am the project manager / UI UX person / frontend developer who also sets up CI/CD build pipelines and manages
-        Linux servers now and then.
-      </p>
-      <p>
-        My main role is frontend development. I am contributing to two legacy applications, one made with ASP.NET MVC 5
-        and the other made with Durandal and Knockout.js. Then i'm working on the upcoming version of the application
-        that's being built with React Native and Typescript.
-      </p>
+      <section>
+        <h3>Current job: Frontend Developer</h3>
+        <p>
+          <Tag>reactjs</Tag> <Tag>react-native</Tag> <Tag>redux</Tag> <Tag>typescript</Tag> <Tag>styled-components</Tag>
+        </p>
+        <p>
+          I am the project manager / UI UX person / frontend developer who also sets up CI/CD build pipelines and
+          manages Linux servers now and then.
+        </p>
+        <p>
+          My main role is frontend development. I am contributing to two legacy applications, one made with ASP.NET MVC
+          5 and the other made with Durandal and Knockout.js. Then i'm working on the upcoming version of the
+          application that's being built with React Native and Typescript.
+        </p>
 
-      <p>
-        I also work on the UI of said application, to the extent where i have designed 20+ screens of the initial
-        release and have built a design prototype for the rest of the frontend team to take lead from.
-      </p>
+        <p>
+          I also work on the UI of said application, to the extent where i have designed 20+ screens of the initial
+          release and have built a design prototype for the rest of the frontend team to take lead from.
+        </p>
 
-      <p>
-        The CI/CD for the React Native project was also setup by me, using Bitbucket Pipelines. It takes the project,
-        builds it, sends it to the app stores and so on.
-      </p>
+        <p>
+          The CI/CD for the React Native project was also setup by me, using Bitbucket Pipelines. It takes the project,
+          builds it, sends it to the app stores and so on.
+        </p>
 
-      <p>
-        On occasion i use the command line to manage Linux servers for WordPress websites using the LAMP stack. This
-        includes installing packages, setting up Apache server, setting up virtual hosts, install SSLs, creating user
-        accounts and changing file and folder permissions.
-      </p>
+        <p>
+          On occasion i use the command line to manage Linux servers for WordPress websites using the LAMP stack. This
+          includes installing packages, setting up Apache server, setting up virtual hosts, install SSLs, creating user
+          accounts and changing file and folder permissions.
+        </p>
 
-      <p>
-        To top it all off, i also oversee management of the various projects the team is working on to make sure
-        everyone is productive. This includes creating and tracking tasks, provide access/information where needed, and
-        hosting standups.
-      </p>
-
-      <nav>
-        <a href="https://stackoverflow.com/users/story/890814">Stackoverflow</a>
-      </nav>
+        <p>
+          To top it all off, i also oversee management of the various projects the team is working on to make sure
+          everyone is productive. This includes creating and tracking tasks, provide access/information where needed,
+          and hosting standups.
+        </p>
+        <nav>
+          <a href="https://stackoverflow.com/users/story/890814">Stackoverflow</a>
+        </nav>
+      </section>
     </DefaultLayout>
   )
 }
